5d0899798f6956457bc5d08afe29887c5488e0c2
Milan Thapa <thapamilan9762@gmail.com>
2025-08-02 06:45:07 +0545

    Add seperate about page for SEO

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '@/components/Header'
+import About from '@/components/About'
+import Footer from '@/components/Footer'
+
+const AboutPage = () => {
+  return (
+    <>
+    <Header/>
+    <About/>
+    <Footer/>
+    
+    </>
+  )
+}
+
+export default AboutPage
