3d40564485e0620f2995d8d1428a4e33c994442c
Milan Thapa <thapamilan9762@gmail.com>
2025-08-02 06:49:42 +0545

    Add Seperate Skills Page for SEO

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Skills from '@/components/Skills'
+import Footer from '@/components/Footer'
+
+const SkillsPage = () => {
+  return (
+   <>
+   <Header/>
+   <Skills/>
+   <Footer/>
+   
+   
+   </>
+  )
+}
+
+export default SkillsPage
