1b8ee0be33e181163a2a72d16dd57c303d3eb84d
Milan Thapa <thapamilan9762@gmail.com>
2025-08-02 06:47:16 +0545

    Add Seperate Project page for SEO

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Projects from '@/components/Projects'
+import Footer from '@/components/Footer'
+
+const ProjectPage = () => {
+  return (
+   <>
+   <Header/>
+   <Projects/>
+   <Footer/>
+   
+   
+   </>
+  )
+}
+
+export default ProjectPage
